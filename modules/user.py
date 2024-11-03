484ebb6696dacd93a42ac2bc6796c4797f7d882c
AlexHunter89 <alex.j.hunter1@gmail.com>
2024-11-03 10:22:44 -0500

    Newlines

--- modules/user.py
+++ modules/user.py
@@ -3,31 +3,25 @@ from rich import print
 from rich.markup import escape
 
 def greet_and_identify_user():
-    #print()
     print("\nWelcome to the inventory tracking system.\n")
-    #print()
     user_name_prompt = "Please enter your name: "
     user_name = pyip.inputStr(prompt=user_name_prompt)
     employee_number_prompt = "Please enter your employee number:"
     employee_number = pyip.inputNum(prompt=employee_number_prompt)
     employee_identity = user_name + ": " + str(employee_number)
-    print()
-    print(f"Welcome: {escape(user_name)}")
+    print(f"\nWelcome: {escape(user_name)}")
     return employee_identity
 
 def warn_user_new_session():
-    new_session_overwrite_warning = "[bold red]WARNING![/bold red] Starting a new session will overwrite any previous sessions."
+    new_session_overwrite_warning = "\n[bold red]WARNING![/bold red] Starting a new session will overwrite any previous sessions.\n"
     user_continuation_prompt = "Are you sure you want to continue? (yes/no): "
-    print()
     print(new_session_overwrite_warning)
-    print()
     user_continuation_response = pyip.inputYesNo(prompt=user_continuation_prompt)
     print()
     return user_continuation_response
 
 def user_change_bin():
-    print()
-    change_bin_prompt = "Would you like to change the current bin you are working on? (yes/no): "
+    change_bin_prompt = "\nWould you like to change the current bin you are working on? (yes/no): "
     change_bin_response = pyip.inputYesNo(prompt=change_bin_prompt)
     return change_bin_response
 
