4578d7d153a34a29bc6b48cef14ea032cfb73986
AlexHunter89 <alex.j.hunter1@gmail.com>
2024-11-03 10:27:54 -0500

    Update menu.py
    Adding doc strings.

--- modules/menu.py
+++ modules/menu.py
@@ -1,6 +1,7 @@
 import pyinputplus as pyip
 
 def display_menu():
+    """Displays the main menu. It shows all of the available actions that the user can take."""
     user_menu_entry = pyip.inputMenu(['Start New Session (Not Recommended)',
                                       'Load Previous Session (Recommended)',
                                       'Delete Entry',
