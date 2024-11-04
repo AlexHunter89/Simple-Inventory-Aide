69337f011a089a802df641f03a43cbf8165b05a4
AlexHunter89 <alex.j.hunter1@gmail.com>
2024-11-04 10:58:49 -0500

    Update menu.py

--- modules/menu.py
+++ modules/menu.py
@@ -13,15 +13,19 @@ except ImportError:
     print("Please install it by running: python -m pip install rich")
     exit(1)
 
+menu_options = [
+    'Start New Session (Not Recommended)',
+    'Load Previous Session (Recommended)',
+    'Delete Entry',
+    'Modify Entry',
+    'Change Current Bin',
+    'Exit'
+    ]
+
 def display_menu(user_identity, current_bin):
     """Displays the main menu. It shows all of the available actions that the user can take.
     Also displays the current active user and current bin."""
     print(f"Current User: [blue]{escape(user_identity)}[/blue]")
     print(f"The current bin is: {escape(current_bin)}")
-    user_menu_entry = pyip.inputMenu(['Start New Session (Not Recommended)',
-                                      'Load Previous Session (Recommended)',
-                                      'Delete Entry',
-                                      'Modify Entry',
-                                      'Change Current Bin',
-                                      'Exit'], numbered=True)
+    user_menu_entry = pyip.inputMenu(menu_options, numbered=True)
     return user_menu_entry
