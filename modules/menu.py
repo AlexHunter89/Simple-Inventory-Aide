0f98688968fa172030541182aa8a2571d6cc861c
AlexHunter89 <alex.j.hunter1@gmail.com>
2024-11-03 21:38:32 -0500

    Update menu.py

--- modules/menu.py
+++ modules/menu.py
@@ -1,7 +1,11 @@
 import pyinputplus as pyip
+from rich import print
+from rich.markup import escape
 
-def display_menu():
-    """Displays the main menu. It shows all of the available actions that the user can take."""
+def display_menu(user_identity):
+    """Displays the main menu. It shows all of the available actions that the user can take.
+    Also displays the current active user."""
+    print(f"Current User: [blue]{escape(user_identity)}[/blue]")
     user_menu_entry = pyip.inputMenu(['Start New Session (Not Recommended)',
                                       'Load Previous Session (Recommended)',
                                       'Delete Entry',
